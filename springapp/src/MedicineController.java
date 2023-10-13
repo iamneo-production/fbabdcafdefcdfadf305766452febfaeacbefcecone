2bdfcd684883f903c8079c8b3374e8feff8292b6
Iamneo <techadmin@iamneo.ai>
2023-10-13 16:58:00 +0000

    Backup Commit

--- springapp/src/MedicineController.java
+++ springapp/src/MedicineController.java
@@ -1,34 +1,46 @@
-package com.example.pharmacy.controller;
 
-import com.example.pharmacy.model.Medicine;
+package controller;
+
+import model.Medicine;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequestMapping("/medicines")
 public class MedicineController {
-    private Map<Integer, Medicine> medicineMap = new ConcurrentHashMap<>();
-    private int nextMedicineId = 1;
+
+    private Map<Integer, Medicine> medicineMap = new HashMap<>();
 
     @PostMapping
     public boolean addMedicine(@RequestBody Medicine medicine) {
-        medicine.setMedicineId(nextMedicineId);
-        medicineMap.put(nextMedicineId, medicine);
-        nextMedicineId++;
-        return true; // Successfully added medicine
+        medicineMap.put(medicine.getMedicineId(), medicine);
+        return true;
     }
 
     @PutMapping("/{medicineId}")
-    public Medicine updateMedicine(@PathVariable int medicineId, @RequestBody Medicine updatedMedicine) {
+    public boolean updateMedicine(@PathVariable int medicineId, @RequestBody Medicine updatedMedicine) {
         if (medicineMap.containsKey(medicineId)) {
-            updatedMedicine.setMedicineId(medicineId);
-            medicineMap.put(medicineId, updatedMedicine);
-            return updatedMedicine;
+            Medicine medicine = medicineMap.get(medicineId);
+            medicine.setMedicineName(updatedMedicine.getMedicineName());
+            medicine.setPrice(updatedMedicine.getPrice());
+            medicine.setQuantity(updatedMedicine.getQuantity());
+            medicine.setDescription(updatedMedicine.getDescription());
+            return true;
         }
-        return null; // Medicine not found
+        return false;
+    }
+
+    @GetMapping("/{medicineId}")
+    public Medicine getMedicine(@PathVariable int medicineId) {
+        return medicineMap.get(medicineId);
+    }
+
+    @GetMapping
+    public List<Medicine> getAllMedicines() {
+        return new ArrayList<>(medicineMap.values());
     }
 }
